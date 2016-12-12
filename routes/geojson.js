e1bc1e3518154861a18ce0c96326326336571aef
TecmoB <todd.bleess@gmail.com>
2016-12-12 10:16:13 -0700

    Port change

--- routes/geojson.js
+++ routes/geojson.js
@@ -390,8 +390,8 @@ var schema = req.query.schema ||  function(){
   return '';  //no valid database - will deal with later 
 }();
   
-  var port='5432';
-  if(db==='acs1014'){port='5433';}
+  var port='5433';
+  //if(db==='acs1014'){port='5433';} All databases now use 5433
   
   conString = "postgres://codemog:demography@gis.dola.colorado.gov:" + port + "/" + db;
   
