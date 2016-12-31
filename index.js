6a23c7894c027e14d91b57a72344bbbe39dcc14d
Daniel Trone <royhobbstn@yahoo.com>
2016-12-31 13:30:08 +0000

    cloud 9 port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 var pg = require('pg');
 var csv = require('express-csv');
 
-var conString = "postgres://codemog:demography@gis.dola.colorado.gov:5433/";
+var conString = "postgres://codemog:demography@acs-export.cudcner3avpd.us-west-2.rds.amazonaws.com:5432/";
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -21,7 +21,7 @@ require('./routes/demog.js')(app, pg, csv, conString);
 require('./routes/geojson.js')(app, pg, csv, conString);
 
 
-var server = app.listen(4002, function() {
+var server = app.listen(8080, function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Example app listening at http://', host, port);
