c0f3b0e655d5b27e109b74bebc1ba6b289dcff7e
Daniel Trone <royhobbstn@yahoo.com>
2017-01-02 13:06:32 +0000

    refactoring

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ var conString = "postgres://" + obj.name + ":" + obj.password + "@" + obj.host +
 var allowCrossDomain = function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET");
-
     next();
 };
 
--- routes/demog.js
+++ routes/demog.js
@@ -1,15 +1,17 @@
-//MICROSERVICE for getting data triggered by an advanced query
-
 //returns from: http://nodejs-server-royhobbstn.c9users.io/demog?db=acs1115&schema=data&table=b19013&moe=yes&geonum=108037,108039&type=json
 /*
 {"source":"acs1115","schema":"data","tablemeta":[{"table_id":"b19013","table_title":"MEDIAN HOUSEHOLD INCOME IN THE PAST 12 MONTHS (IN 2015 INFLATION-ADJUSTED DOLLARS)","universe":"Universe:  Households"}],"fieldmeta":[{"column_id":"b19013001","column_title":"Median household income in the past 12 months (in 2015 Inflation-adjusted dollars)"}],"data":[{"geoname":"Eagle County, Colorado","state":"8","county":"37","place":null,"tract":null,"bg":null,"geonum":"108037","b19013001":"72214","b19013_moe001":"3789"},{"geoname":"Elbert County, Colorado","state":"8","county":"39","place":null,"tract":null,"bg":null,"geonum":"108039","b19013001":"84963","b19013_moe001":"4050"}],"error":[]}
 */
 
+
+// TODO pretty print option
+
 var csv = require("express-csv");
+var winston = require('winston');
 
 var strpos = require("locutus/php/strings/strpos");
 var substr_replace = require("locutus/php/strings/substr_replace");
-var explode = require("locutus/php/strings/explode");
+
 var substr = require("locutus/php/strings/substr");
 var implode = require("locutus/php/strings/implode");
 var array_merge = require("locutus/php/array/array_merge");
@@ -20,45 +22,38 @@ module.exports = function (app, pg, conString) {
 
   app.get('/demog', function (req, res) {
 
+    // notification of bad param in error output. 
+    // TODO if !json, this error is swallowed.  not ideal.
+    checkValidParams(req.query, errorarray);
+
+
     //potential multi select (comma delimited list)
-    var field = req.query.field || "undefined";
-    var state = req.query.state || "undefined";
-    var county = req.query.county || "undefined";
-    var geonum = req.query.geonum || "undefined";
-    var geoid = req.query.geoid || "undefined";
-    var sumlev = req.query.sumlev || "undefined";
-    var table = req.query.table || "undefined";
+    var field = req.query.field;
+    var state = req.query.state;
+    var county = req.query.county;
+    var geonum = req.query.geonum;
+    var geoid = req.query.geoid;
+    var sumlev = req.query.sumlev;
+    var table = req.query.table;
 
 
     //potential single select
     var type = req.query.type || 'json';
     var db = req.query.db || 'acs1115';
-    //set default for schema if it is missing
-    var schema = req.query.schema || function () {
-      if (db === 'acs1115' || db === 'acs1014' || db === 'acs0913' || db === 'acs0812' || db === 'c2010') {
-        return 'data';
-      }
-      if (db === 'c2000' || db === 'c1990' || db === 'c1980') {
-        return 'sf1';
-      }
-      return ''; //no valid database - will deal with later 
-    }();
 
-    conString = "postgres://codemog:demography@acs-export.cudcner3avpd.us-west-2.rds.amazonaws.com:5432/" + db;
-    var geo = req.query.geo || "undefined";
-    var series = req.query.series || "undefined";
-    var type = req.query.type || "undefined";
+    if (!validateDB(db, errorarray)) {
+      res.send('Database parameter ' + db + ' is not a valid database!');
+      return;
+    }
+
+    //set default for schema if it is missing
+    var schema = req.query.schema || setDefaultSchema(db, res);
+
+
     //by default limits to 1000 search results.  override by setting limit= in GET string
     var limit = parseInt(req.query.limit, 10) || 1000;
 
-    //if database is acs, check to see if moe option is flagged
-    var moe = 'no';
-    if (db === 'acs0812' || db === 'acs0913' || db === 'acs1014' || db === 'acs1115') {
-      if (req.query.moe) {
-        moe = req.query.moe;
-      }
-    }
-
+    var moe = getMOE(db, req.query.moe);
 
     //declare useful vars
     var fullarray = []; //final data array
@@ -70,90 +65,50 @@ module.exports = function (app, pg, conString) {
     var tablelist = []; //array of all tables used in query
     var jointablelist = ""; //working string of tables to be inserted into sql query
     var joinlist = ""; //working string of 'where' condition to be inserted into sql query
-    var arr = [];
-    var arr2 = [];
+
     var moefields = []; //moe field array
     var tcolumns = []; //columns gathered from table(s?)
 
-    //get list of all parameters - check all are valid
-    var getkey = [];
-
-    //<>
     var metacsv = []; //array for csv field descriptions only
-    //<>
     var ttlfields = [];
 
     var lastbranchdone = 0;
 
 
-    //push $_POST vars into simple array for each $key
 
-    //array_push($getkey,$key);  
-
-    for (var propName in req.query) {
-      if (req.query.hasOwnProperty(propName)) {
-        getkey.push(propName);
-      }
-    }
-
-
-    //loop through $keys, check against list of valid values
-    for (var i = 0; i < getkey.length; i++) {
-      if (getkey[i] !== 'field' && getkey[i] !== 'state' && getkey[i] !== 'county' && getkey[i] !== 'sumlev' && getkey[i] !== 'place' && getkey[i] !== 'geonum' && getkey[i] !== 'geoid' && getkey[i] !== 'db' && getkey[i] !== 'schema' && getkey[i] !== 'geo' && getkey[i] !== 'series' && getkey[i] !== 'type' && getkey[i] !== 'limit' && getkey[i] !== 'moe' && getkey[i] !== 'table' && getkey[i] !== 'type') {
-        errorarray.push('Your parameter -' + getkey[i] + '- is not valid.');
-      }
-    }
-
-    //validate database selected
-    if (db !== 'c1980' && db !== 'c1990' && db !== 'c2000' && db !== 'c2010' && db !== 'acs0812' && db !== 'acs0913' && db !== 'acs1014' && db !== 'acs1115') {
-      errorarray.push('Your database choice `' + db + '` is not valid.');
-      db = "";
-      return 'error'; //goto a;
-    }
 
 
     //if no fields or tables are selected
-    if (table === "undefined" && field === "undefined") {
-      errorarray.push('You need to specify a table or fields to query.');
-      return 'error'; //goto a;
+    if (!table && !field) {
+      res.send('You need to specify a table or fields to query.');
+      return;
     }
 
-    //as far as errors go, tell people they are wasting their time specifying table(s) if they specified field.
-    if (field !== "undefined" && table !== "undefined") {
+    //as far as errors go, tell people they are wasting their time specifying table(s) if they already specified field.
+    if (field && table) {
       errorarray.push('You specified TABLE.  This parameter is ignored when you also specify FIELD');
     }
 
 
     //if no fields are selected (then a table must be).  Create fields list based on the given table.
-    if (field === "undefined") {
+    if (!field) {
 
-      var atablearray = explode(",", table);
-
-
-      var atablestr = "";
-
-      for (var j = 0; j < atablearray.length; j++) {
-        atablestr = atablestr + " table_name='" + atablearray[j] + "' or";
-      }
-
-      //trim last trailing 'or'
-      atablestr = substr(atablestr, 0, -2);
+      var table_list = sqlList(table, 'table_name');
 
       //Query table fields --ONLY SINGLE TABLE SELECT AT THIS TIME--
-      var tablesql = "SELECT column_name from information_schema.columns where (" + atablestr + ") and table_schema='" + schema + "';";
+      var tablesql = "SELECT column_name from information_schema.columns where (" + table_list + ") and table_schema='" + schema + "';";
 
       field = sendinternal(tablesql, 1); //ASYNC
 
     }
 
 
+
     function check() {
-      if (field === undefined || field === "undefined") {
+      if (!field) {
         setTimeout(check, 50);
-        //console.log('waiting...');
       }
       else {
-        //console.log('field: '+field);
         continue_program();
         main_logic();
       }
@@ -170,13 +125,12 @@ module.exports = function (app, pg, conString) {
 
       // we have fields: either hand entered or derived from tables
 
-
       //break the comma delimited records from field into an array  
-      ttlfields = explode(",", field);
+      ttlfields = field.split(",");
 
 
       //if moe is set to yes, add the moe version of each field (push into new array, then merge with existing)
-      if (moe === 'yes') {
+      if (moe) {
 
         var pos;
 
@@ -247,81 +201,72 @@ module.exports = function (app, pg, conString) {
 
 
     function main_logic() {
+
+      var joinlist;
+
       //CASE 1:  you have a geonum
       //essentially you don't care about anything else.  just get the data for that/those geonum(s)
-      if (geonum !== "undefined") {
+      if (geonum) {
 
         //as far as errors go, tell people they are wasting their time specifying sumlev, state, county, place and geoid if they specified geonum.
-        if (state !== "undefined") {
+        if (state) {
           errorarray.push('You specified STATE.  This parameter is ignored when you also specify GEONUM');
-        };
-        if (county !== "undefined") {
+        }
+        if (county) {
           errorarray.push('You specified COUNTY.  This parameter is ignored when you also specify GEONUM');
-        };
-        if (sumlev !== "undefined") {
+        }
+        if (sumlev) {
           errorarray.push('You specified SUMLEV.  This parameter is ignored when you also specify GEONUM');
-        };
-        if (geoid !== "undefined") {
+        }
+        if (geoid) {
           errorarray.push('You specified GEOID.  This parameter is ignored when you also specify GEONUM');
-        };
-
-        //break the comma delimited records from geonum into an array  
-        var geonumarray = explode(",", geonum);
-
-        //iterate through all geonum's
-        for (var z = 0; z < geonumarray.length; z++) {
-          joinlist = joinlist + " geonum=" + geonumarray[z] + " or";
         }
 
-
-        //trim last trailing 'or'
-        joinlist = substr(joinlist, 0, -2);
+        joinlist = sqlList(geonum, 'geonum');
 
         //END CASE 1
       }
-      else if (geoid !== "undefined") {
+      else if (geoid) {
         //CASE 2:  you have a geoid
 
         //as far as errors go, tell people they are wasting their time specifying sumlev, state, county, place and geoid if they specified geonum.
-        if (state !== "undefined") {
+        if (state) {
           errorarray.push('You specified STATE.  This parameter is ignored when you also specify GEOID');
-        };
-        if (county !== "undefined") {
-          errorarray.push('You specified COUNTY.  This parameter is ignored when you also specify GEOID');
-        };
-        if (sumlev !== "undefined") {
-          errorarray.push('You specified SUMLEV.  This parameter is ignored when you also specify GEOID');
-        };
-
-        //break the comma delimited records from geonum into an array  
-        var geoidarray = explode(",", geoid);
-
-        //iterate through all geoids, simply put a '1' in front and treat them like geonums
-        for (var y = 0; y < geoidarray.length; y++) {
-          joinlist = joinlist + " geonum=1" + geoidarray[y] + " or";
         }
-        //trim last trailing 'or'
-        joinlist = substr(joinlist, 0, -2);
+        if (county) {
+          errorarray.push('You specified COUNTY.  This parameter is ignored when you also specify GEOID');
+        }
+        if (sumlev) {
+          errorarray.push('You specified SUMLEV.  This parameter is ignored when you also specify GEOID');
+        }
+
+
+        joinlist = sqlListGeoID(geoid, "geonum");
+
+        console.log(joinlist);
 
         //END CASE 2  
       }
-      else if (sumlev !== "undefined" || county !== "undefined" || state !== "undefined") {
+      else if (sumlev || county || state) {
         //CASE 3 - query
 
         var condition = ""; //condition is going to be a 3 character string which identifies sumlev, county, state (yes/no) (1,0)
-        if (sumlev !== "undefined") {
+
+        if (sumlev) {
           condition = "1";
         }
         else {
           condition = "0";
         }
-        if (county !== "undefined") {
+
+        if (county) {
           condition = condition + "1";
         }
         else {
           condition = condition + "0";
         }
-        if (state !== "undefined") {
+
+        if (state) {
           condition = condition + "1";
         }
         else {
@@ -329,55 +274,22 @@ module.exports = function (app, pg, conString) {
         }
 
 
-
-        if (county !== "undefined") {
+        if (county) {
           //create county array out of delimited list
-          var countylist = "";
-
-          //break the comma delimited records from county into an array  
-          var countyarray = explode(",", county);
-
-          //iterate through all counties
-          for (var x = 0; x < countyarray.length; x++) {
-            countylist = countylist + " county=" + countyarray[x] + " or";
-          }
-
-          //trim last trailing 'or'
-          countylist = substr(countylist, 0, -2);
+          var countylist = sqlList(county, 'county');
         }
 
 
-        if (state !== "undefined") {
+        if (state) {
           //create state array out of delimited list
-          var statelist = "";
-
-          //break the comma delimited records from county into an array  
-          var statearray = explode(",", state);
-
-          //iterate through all states
-          for (var u = 0; u < statearray.length; u++) {
-            statelist = statelist + " state=" + statearray[u] + " or";
-          }
-
-          //trim last trailing 'or'
-          statelist = substr(statelist, 0, -2);
+          var statelist = sqlList(state, 'state');
         }
 
+        // TODO warning sumlev is STATE 40 but county is given
 
-        if (sumlev !== "undefined") {
+        if (sumlev) {
           //create sumlev array out of delimited list
-          var sumlevlist = "";
-
-          //break the comma delimited records from county into an array  
-          var sumlevarray = explode(",", sumlev);
-
-          //iterate through all sumlevs
-          for (var v = 0; v < sumlevarray.length; v++) {
-            sumlevlist = sumlevlist + " sumlev=" + sumlevarray[v] + " or";
-          }
-
-          //trim last trailing 'or'
-          sumlevlist = substr(sumlevlist, 0, -2);
+          var sumlevlist = sqlList(sumlev, 'sumlev');
         }
 
         //every possible combination of sumlev, county, state
@@ -465,13 +377,6 @@ module.exports = function (app, pg, conString) {
             notobject.push(interarr);
           }
 
-          //header("Content-Type: text/csv");
-          //header("Content-Disposition: attachment; filename=file.csv");
-          // Disable caching
-          //header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1
-          //header("Pragma: no-cache"); // HTTP 1.0
-          //header("Expires: 0"); // Proxies
-
           //add geonum to front of fields row array
           array_unshift(ttlfields, "geonum");
           array_unshift(ttlfields, "bg");
@@ -494,7 +399,7 @@ module.exports = function (app, pg, conString) {
           array_unshift(notobject, ttlfields);
 
           //console.log(notobject);
-          res.setHeader('Content-disposition', 'attachment; filename=CO_DemogExport.csv')
+          res.setHeader('Content-disposition', 'attachment; filename=CO_DemogExport.csv');
           res.csv(notobject);
 
         }
@@ -514,7 +419,6 @@ module.exports = function (app, pg, conString) {
       return;
     }
 
-
     return;
 
 
@@ -523,8 +427,8 @@ module.exports = function (app, pg, conString) {
     function sendtodatabase(sqlstring) {
 
       console.log(sqlstring);
-      var client = new pg.Client(conString);
-      console.log("a:", conString);
+      var client = new pg.Client(conString + db);
+      console.log("a:", conString + db);
 
       client.connect(function (err) {
 
@@ -576,8 +480,8 @@ module.exports = function (app, pg, conString) {
     function sendinternal(sqlstring, branch) {
 
       console.log(sqlstring);
-      var client = new pg.Client(conString);
-      console.log("b:", conString);
+      var client = new pg.Client(conString + db);
+      console.log("b:", conString + db);
 
       client.connect(function (err) {
 
@@ -675,4 +579,128 @@ module.exports = function (app, pg, conString) {
 
   });
 
+};
+
+
+
+
+
+function checkValidParams(query, error) {
+
+  //get list of all parameters - check all are valid
+  var getkey = [];
+
+  //push $_POST vars into simple array for each $key
+  for (var propName in query) {
+    if (query.hasOwnProperty(propName)) {
+      getkey.push(propName);
+    }
+  }
+
+  //loop through $keys, check against list of valid values
+  for (var i = 0; i < getkey.length; i++) {
+    if (getkey[i] !== 'field' && getkey[i] !== 'state' && getkey[i] !== 'county' && getkey[i] !== 'sumlev' && getkey[i] !== 'place' && getkey[i] !== 'geonum' && getkey[i] !== 'geoid' && getkey[i] !== 'db' && getkey[i] !== 'schema' && getkey[i] !== 'geo' && getkey[i] !== 'series' && getkey[i] !== 'type' && getkey[i] !== 'limit' && getkey[i] !== 'moe' && getkey[i] !== 'table') {
+      error.push('Your parameter -' + getkey[i] + '- is not valid.');
+    }
+  }
+
+}
+
+
+
+function makeRequest(method, url) {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = function () {
+      if (this.status >= 200 && this.status < 300) {
+        resolve(xhr.response);
+      }
+      else {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText
+        });
+      }
+    };
+    xhr.onerror = function () {
+      reject({
+        status: this.status,
+        statusText: xhr.statusText
+      });
+    };
+    xhr.send();
+  });
+}
+
+
+
+function sqlList(str_list, field_name) {
+
+  var list_array = str_list.split(",");
+
+  var str = "";
+
+  list_array.forEach(function (d) {
+    str = str + " " + field_name + "='" + d + "' or";
+  });
+
+  console.log(str.slice(0, -2));
+  //trim last trailing 'or'
+  return str.slice(0, -2);
+
+}
+
+
+function sqlListGeoID(str_list, field_name) {
+
+  var list_array = str_list.split(",");
+
+  var str = "";
+
+  list_array.forEach(function (d) {
+    str = str + " " + field_name + "='1" + d + "' or";
+  });
+
+  console.log(str.slice(0, -2));
+  //trim last trailing 'or'
+  return str.slice(0, -2);
+
+}
+
+
+function validateDB(db, error) {
+  //validate database
+  if (db !== 'c1980' && db !== 'c1990' && db !== 'c2000' && db !== 'c2010' && db !== 'acs0812' && db !== 'acs0913' && db !== 'acs1014' && db !== 'acs1115') {
+    error.push('Your database choice `' + db + '` is not valid.');
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+
+function setDefaultSchema(db) {
+  // db already validated.  setting default to 'data' ensures easier compatibility with future ACS releases.
+  if (db === 'c2000' || db === 'c1990' || db === 'c1980') {
+    return 'sf1';
+  }
+  else {
+    return 'data';
+  }
+}
+
+
+
+
+function getMOE(db, moe) {
+  //if database is acs, check to see if moe option is flagged
+  // TODO, just check first three letters of db
+  if (db === 'acs0812' || db === 'acs0913' || db === 'acs1014' || db === 'acs1115') {
+    if (moe === 'yes' || moe === 'y' || moe === 'true') {
+      return true;
+    }
+  }
+  return false;
 }
